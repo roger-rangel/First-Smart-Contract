5820bdd0389bfc267af9c20fbae4a6bfd5e0691d
Roger Rangel <roger.rangel.ee@gmail.com>
2022-03-27 09:04:02 +0200

    Deployment to Rinkeby

--- deploy.js
+++ deploy.js
@@ -9,3 +9,18 @@ const provider = new HDWalletProvider(
     'https://rinkeby.infura.io/v3/e3ccfbb825d049578995b7586c5fecd4'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("attempting to deploy from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0]});
+
+    console.log("contract deployed to", result.options.address);
+    provider.engine.stop()
+
+}
+deploy();
